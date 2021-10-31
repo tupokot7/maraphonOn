c200254ccabe40c90ef84eab2671f94d55e930ea
madKot <madKot@gmail.com>
2021-10-31 21:33:02 +0300

    added in .js calc

--- hello/script.js
+++ hello/script.js
@@ -1,10 +1,10 @@
-let a = prompt("Введите первое число:");
-let op = prompt("Введите одно из: '+' '-' '*' '/'");
-let b = prompt("Введите второе число:");
+let a = +prompt("Введите первое число: ");
+let op = prompt("Введите одно из: +, -, * или /");
+let b = +prompt("Введите второе число: ");
 let result;
 
    // ----- т.к. до switch не дошли - делаю по ТЗ по пройденным темам :) -----
-function result() {
+function calc() {
    if (op === "+") {
       result = a + b;
    } else if(op === "-") {
@@ -15,9 +15,7 @@ function result() {
       result = a / b;
    } else if (op != "+","-","*","/") {
       alert("Error: unknown operation");
-   } else if (a || b != Number) {
-      alert("Error")
    }
    alert("Результат: " + result);
 }
-result();
+calc();
